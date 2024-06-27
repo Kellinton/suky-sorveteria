f02b1824c5c7db900a902a0f610d1788bd73ae2e
Kellinton <kellintonbrasil@gmail.com>
2024-06-27 16:12:52 -0300

    feat: retornando dados pela api na tela dashboard

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -55,40 +55,4 @@ class AdministradorController extends Controller
         ));
     }
 
-    public function login(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'senha' => 'required',
-        ]);
-
-        $usuario = Usuario::where('email', $credentials['email'])->where('senha', $credentials['senha'])->first();
-
-        if ($usuario && $usuario->tipo_usuario_type === 'administrador') {
-            $administrador = $usuario->tipo_usuario()->first();
-            if ($administrador) {
-
-                $token = $usuario->createToken('Token de Acesso')->plainTextToken;
-
-                return response()->json([
-                    'message' => 'Login bem sucedido!',
-                    'usuario' => [
-                        'id'    => $usuario->id,
-                        'nome'  => $usuario->nome,
-                        'email' => $usuario->email,
-                        'tipo_usuario'  => $usuario->tipo_usuario_type,
-                        'dados_administrador'   => [
-                            'idAdministrador'   => $administrador->id,
-                            'nome'      => $administrador->nomeFuncionario,
-                        ],
-                    ],
-
-                    'access_token' => $token,
-                    'token_type'  => 'Bearer',
-                ]);
-            }
-        }
-        return response()->json(['message' => 'Credenciais inválidas ou usuário não é administrador'], 401);
-    }
-
 }
--- app/Http/Controllers/api/DashboardController.php
+++ app/Http/Controllers/api/DashboardController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Funcionario;
+use App\Models\Contato;
+use App\Models\Produto;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,11 +17,37 @@ class DashboardController extends Controller
     public function index($idFuncionario)
     {
         // Buscar os dados do funcionário
-        $funcionario = Funcionario::findOrFail($idFuncionario); // Ajuste se estiver utilizando outro método
+        $funcionario = Funcionario::findOrFail($idFuncionario);
 
         $tipoFuncionario = ucfirst($funcionario->tipo_funcionario);
 
+
         $fotoUrl = asset('img/funcionarios/' . $funcionario->fotoFuncionario);
+
+        // Quantidade valor em produtos
+        $totalValorProdutos = Produto::sum('valorProduto');
+
+        // Formatar o valor como moeda
+        $totalValorProdutos = number_format($totalValorProdutos, 2, ',', '.');
+
+        // Produtos Totais
+        $totalProdutos = Produto::count();
+
+        // Funcionários Totais
+        $totalFuncionarios = Funcionario::count();
+
+        // Buscar as 3 mensagens mais recentes
+        $mensagensRecentes = Contato::orderBy('created_at', 'desc')->take(3)->get();
+
+        // Formatar as mensagens e datas
+        $mensagensRecentes = $mensagensRecentes->map(function($mensagem) {
+            return [
+                'nomeContato' => $mensagem->nomeContato,
+                'mensagemContato' => Str::limit($mensagem->mensagemContato, 25, '...'),
+                'created_at' => Carbon::parse($mensagem->created_at)->diffForHumans(),
+            ];
+        });
+
 
         return response()->json([
             'dadosFuncionario' => [
@@ -25,7 +55,12 @@ class DashboardController extends Controller
                 'sobrenome_funcionario' => $funcionario->sobrenomeFuncionario,
                 'foto_funcionario' => $fotoUrl,
                 'tipo_funcionario' => $tipoFuncionario,
-            ]
+            ],
+            'totalValorProdutos' => $totalValorProdutos,
+            'totalProdutos' => $totalProdutos,
+            'totalFuncionarios' => $totalFuncionarios,
+            'mensagensRecentes' => $mensagensRecentes,
+
         ]);
     }
 }
