01b9785b8e1f6b7f5881151c90d6d0242066e3ec
Kellinton <kellintonbrasil@gmail.com>
2024-06-28 19:42:21 -0300

    feat: recursos de acessibilidade: ajuste de tamanho de fonte, modo de alto contraste e integração com VLibras

--- resources/views/dashboard/layoutdash/index.blade.php
+++ resources/views/dashboard/layoutdash/index.blade.php
@@ -30,6 +30,23 @@
   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
   @livewireStyles
+
+  <style>
+    /* Estilos para modo de alto contraste */
+    .alto-contraste *{
+        color: #fff !important;
+        background-color: #000 !important;
+    }
+
+    .alto-contraste button,
+    .alto-contraste input {
+        background-color: #333 !important;
+        border-color: #fff !important;
+        color: #fff !important;
+    }
+
+  </style>
+
 </head>
 
 <body class="g-sidenav-show bg-gray-100 ">
@@ -366,72 +383,54 @@
   </div>
 
   <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-      <i class="ri-message-3-fill"></i>
+    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2" style="background-color: #596CFF;">
+        <i class="ri-eye-fill text-white"></i>
     </a>
-    <div class="card shadow-lg ">
-      <div class="card-header pb-0 pt-3 ">
-        <div class="float-start">
-          <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
-          <p>See our dashboard options.</p>
-        </div>
-        <div class="float-end mt-4">
-          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-            <i class="fa fa-close"></i>
-          </button>
-        </div>
-        <!-- End Toggle Button -->
-      </div>
-      <hr class="horizontal dark my-1">
-      <div class="card-body pt-sm-3 pt-0">
-        <!-- Sidebar Backgrounds -->
-        <div>
-          <h6 class="mb-0">Sidebar Colors</h6>
-        </div>
-        <a href="javascript:void(0)" class="switch-trigger background-color">
-          <div class="badge-colors my-2 text-start">
-            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-          </div>
-        </a>
-        <!-- Sidenav Type -->
-        <div class="mt-3">
-          <h6 class="mb-0">Sidenav Type</h6>
-          <p class="text-sm">Choose between 2 different sidenav types.</p>
-        </div>
-        <div class="d-flex">
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-        </div>
-        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-        <!-- Navbar Fixed -->
-        <div class="mt-3">
-          <h6 class="mb-0">Navbar Fixed</h6>
-        </div>
-        <div class="form-check form-switch ps-0">
-          <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-        </div>
-        <hr class="horizontal dark my-sm-4">
-        <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/soft-ui-dashboard">Free Download</a>
-        <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard">View documentation</a>
-        <div class="w-100 text-center">
-          <a class="github-button" href="https://github.com/creativetimofficial/soft-ui-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
-          <h6 class="mt-3">Thank you for sharing!</h6>
-          <a href="https://twitter.com/intent/tweet?text=Check%20Soft%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-          </a>
-          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
 
+    <div class="card shadow-lg accessibility-menu">
+        <div class="card-header pb-0 pt-3">
+            <div class="float-end mt-4">
+                <button class="btn btn-link bg-primary text-white p-3 text-dark p-0 fixed-plugin-close-button">
+                    <i class="ri-close-line ri-xl"></i>
+                </button>
+            </div>
+            <div class="float-start">
+                <h5 class="mt-3 mb-0">Acessibilidade</h5>
+                <p>Opções de acessibilidade para melhorar sua experiência.</p>
+            </div>
+        </div>
+        <hr class="horizontal dark my-1">
+        <div class="card-body pt-sm-3 pt-0">
+            <!-- Opções de Acessibilidade -->
+            <div>
+                <h6 class="mb-0">Configurações de Acessibilidade</h6>
+            </div>
+            <!-- Controle Deslizante para Aumento de Fonte -->
+            <div class="mb-3">
+                <label for="fontSlider" class="form-label">Ajuste de Tamanho da Fonte:</label>
+                <input type="range" class="form-range" id="fontSlider" min="10" max="32" step="1" value="16" onchange="alterarTamanhoFonte(this.value)">
+            </div>
+            <!-- Botões de Acessibilidade -->
+            <div class="d-flex flex-column">
+                <button class="btn bg-gradient-primary w-100 px-3 mb-2" onclick="altoContraste()">
+                    <i class="ri-adjust-line ri-fw"></i> Alto Contraste
+                </button>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+  </div>
+    <!-- Vlibras -->
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
   <!-- Script -->
   <script src="{{ asset('dashboard/js/main.js') }}"></script>
   <!--   Core JS Files   -->
@@ -447,6 +446,13 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- biblioteca Axios para fazer requisições AJAX -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- VLibras (acessibilidade) -->
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+
+
 
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
@@ -456,11 +462,37 @@
       }
       Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
+
+
+    // Função para alterar o tamanho da fonte baseado no valor do slider
+    function alterarTamanhoFonte(valor) {
+        // Ajusta o tamanho da fonte do body
+        document.body.style.fontSize = valor + 'px';
+
+        // Ajusta o tamanho da fonte para elementos específicos
+        const elementosEspecificos = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6');
+        elementosEspecificos.forEach(elemento => {
+            elemento.style.fontSize = valor + 'px'; // Aplica o tamanho da fonte
+        });
+    }
+
+    // Função para alternar o modo de alto contraste
+    function altoContraste() {
+        // Selecionar o body
+        let body = document.body;
+
+        // Adicionar ou remover a classe 'alto-contraste' para alternar o estilo
+        body.classList.toggle('alto-contraste');
+    }
+
+
   </script>
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="{{ asset('dashboard/js/soft-ui-dashboard.min.js?v=1.0.7') }}"></script>
+
+
 
   <!-- Script para funcionamento do livewire-->
   @livewireScripts
